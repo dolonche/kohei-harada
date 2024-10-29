b06ae67a1135e45e1e97860fbbd151602f0e5912
dolonche <ivan.pitenin@gmail.com>
2024-10-29 19:13:39 +0900

    coming soon

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+
+import tailwind from '@astrojs/tailwind';
+
+import relativeLinks from 'astro-relative-links';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), relativeLinks()]
+});
